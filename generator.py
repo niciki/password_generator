74d468e27d87c5b8d92d1b01ce39a01f5388e47e
niciki <71276784+niciki@users.noreply.github.com>
2021-08-12 00:40:46 +0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -2,39 +2,39 @@ import random, string
 count, length = int(input()), int(input())
 l = length
 for i in range(count):
-    s = set()
-    l1, l2, l3 = [0 for i in range(3)]
-    while l3 == 0 or l3 > l * 2 / 5:
+    set_of_chars_of_password = set()
+    length_uppercase, length_lowercase, length_integers = [0 for i in range(3)]
+    while length_integers == 0 or length_integers > l * 2 / 5:
         length = l
-        l1 = random.randint(length // 8, length - int(length * 2 /3))
-        length -= l1
-        l2 = random.randint(length // 4, length // 2)
-        length -= l2
-        l3 = length
-    for i in range(l1):
+        length_uppercase = random.randint(length // 8, length - int(length * 2 /3))
+        length -= length_uppercase
+        length_lowercase = random.randint(length // 4, length // 2)
+        length -= length_lowercase
+        length_integers = length
+    for i in range(length_uppercase):
         if i <= len(list(set(string.ascii_uppercase) - {'O', 'I'})):
             q = random.choice(list(set(string.ascii_uppercase) - {'O', 'I'}))
-            while q in s:
+            while q in set_of_chars_of_password:
                 q = random.choice(list(set(string.ascii_uppercase) - {'O', 'I'}))
-            s.add(q)
+            set_of_chars_of_password.add(q)
         else:
-            s.add(random.choice(list(set(string.ascii_uppercase) - {'O', 'I'})))
-    for i in range(l2):
+            set_of_chars_of_password.add(random.choice(list(set(string.ascii_uppercase) - {'O', 'I'})))
+    for i in range(length_lowercase):
         if i <= len(list(set(string.ascii_lowercase) - {'o', 'l'})):
             q = random.choice(list(set(string.ascii_lowercase) - {'o', 'l'}))
-            while q in s:
+            while q in set_of_chars_of_password:
                 q = random.choice(list(set(string.ascii_lowercase) - {'o', 'l'}))
-            s.add(q)
+            set_of_chars_of_password.add(q)
         else:
-            s.add(random.choice(list(set(string.ascii_lowercase) - {'o', 'l'})))
-    for i in range(l3):
+            set_of_chars_of_password.add(random.choice(list(set(string.ascii_lowercase) - {'o', 'l'})))
+    for i in range(length_integers):
         if i <= 7:
             q = str(random.choice([2, 3, 4, 5, 6, 7, 8, 9]))
-            while q in s:
+            while q in set_of_chars_of_password:
                 q = str(random.choice([2, 3, 4, 5, 6, 7, 8, 9]))
-            s.add(q)
+            set_of_chars_of_password.add(q)
         else:
-            s.append(str(random.choice([2, 3, 4, 5, 6, 7, 8, 9])))
-    s = list(s)
-    random.shuffle(s)
-    print(''.join(s))
+            set_of_chars_of_password.append(str(random.choice([2, 3, 4, 5, 6, 7, 8, 9])))
+    list_of_chars_of_password = list(set_of_chars_of_password)
+    random.shuffle(list_of_chars_of_password)
+    print(''.join(list_of_chars_of_password))

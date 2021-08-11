bea39e0011720875b51a436fa700afff63442017
niciki <71276784+niciki@users.noreply.github.com>
2021-08-11 23:56:43 +0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -2,7 +2,7 @@ import random, string
 count, length = int(input()), int(input())
 l = length
 for i in range(count):
-    s = []
+    s = set()
     length = l
     l1 = random.randint(1, length - 2)
     length -= l1
@@ -10,10 +10,29 @@ for i in range(count):
     length -= l2
     l3 = length
     for i in range(l1):
-        s.append(random.choice(list(set(string.ascii_uppercase) - {'O', 'I'})))
+        if i <= len(list(set(string.ascii_uppercase) - {'O', 'I'})):
+            q = random.choice(list(set(string.ascii_uppercase) - {'O', 'I'}))
+            while q in s:
+                q = random.choice(list(set(string.ascii_uppercase) - {'O', 'I'}))
+            s.add(q)
+        else:
+            s.add(random.choice(list(set(string.ascii_uppercase) - {'O', 'I'})))
     for i in range(l2):
-        s.append(random.choice(list(set(string.ascii_lowercase) - {'o', 'l'}))) 
+        if i <= len(list(set(string.ascii_lowercase) - {'o', 'l'})):
+            q = random.choice(list(set(string.ascii_lowercase) - {'o', 'l'}))
+            while q in s:
+                q = random.choice(list(set(string.ascii_lowercase) - {'o', 'l'}))
+            s.add(q)
+        else:
+            s.add(random.choice(list(set(string.ascii_lowercase) - {'o', 'l'})))
     for i in range(l3):
-        s.append(str(random.choice([2, 3, 4, 5, 6, 7, 8, 9])))
+        if i <= 7:
+            q = str(random.choice([2, 3, 4, 5, 6, 7, 8, 9]))
+            while q in s:
+                q = str(random.choice([2, 3, 4, 5, 6, 7, 8, 9]))
+            s.add(q)
+        else:
+            s.append(str(random.choice([2, 3, 4, 5, 6, 7, 8, 9])))
+    s = list(s)
     random.shuffle(s)
     print(''.join(s))
